73109e1fbab96cd7dd26bc9951daa30cad6371ad
Ahmed7749 <ahmadkmm2@gmail.com>
2025-12-28 14:16:56 +0300

    created 3 main methods to reduce repetitive code as much as possible

--- src/com/hospital/daos/GenericDAO.java
+++ src/com/hospital/daos/GenericDAO.java
@@ -1,10 +1,15 @@
 package com.hospital.daos;
 
 import com.hospital.utils.DatabaseConnector;
+import com.hospital.utils.RowMapper;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public abstract class GenericDAO<T>{
     protected boolean executeUpdate(String sql, Object... parameters){
@@ -19,6 +24,53 @@ public abstract class GenericDAO<T>{
                 return stmt.executeUpdate() > 0;
             }
         } catch(SQLException e){
+            throw new RuntimeException("Error in db", e);
+        } finally {
+            DatabaseConnector.returnConnection(con);
+        }
+    }
+
+
+    protected Optional<T> executeQuerySingle(String sql, RowMapper<T> mapper, Object... parameters){
+        Connection con = null;
+        try{
+            con = DatabaseConnector.getDatabaseConnection();
+            try(PreparedStatement stmt = con.prepareStatement(sql)){
+                for(int i = 0; i < parameters.length; i++){
+                    stmt.setObject(i + 1, parameters[i]);
+                }
+                ResultSet resultSet = stmt.executeQuery();
+                if(resultSet.next()){
+                    return Optional.of(mapper.mapRow(resultSet));
+                } else{
+                    return Optional.empty();
+                }
+                }
+            }
+        catch (SQLException e){
+            throw new RuntimeException("Db error", e);
+        } finally {
+            DatabaseConnector.returnConnection(con);
+        }
+    }
+
+
+    protected <T> List<T> executeQueryList(String sql, RowMapper<T> mapper, Object... parameters){
+        Connection con = null;
+        try{
+            con = DatabaseConnector.getDatabaseConnection();
+            try(PreparedStatement stmt = con.prepareStatement(sql)){
+                for(int i = 0; i < parameters.length; i++){
+                    stmt.setObject(i + 1, parameters[i]);
+                }
+                ResultSet resultSet = stmt.executeQuery();
+                List<T> list = new ArrayList<>();
+                while(resultSet.next()){
+                    list.add(mapper.mapRow(resultSet));
+                }
+                return list;
+            }
+        } catch (SQLException e){
             throw new RuntimeException("Error in db", e);
         } finally {
             DatabaseConnector.returnConnection(con);
